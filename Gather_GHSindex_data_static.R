1edc6333447852869514e7143a2875fb416eb759
Chris <chris@Chris-As-MacBook-Air.local>
2020-03-21 09:38:49 -0600

    Chris making static variables

--- Gather_GHSindex_data_static.R
+++ Gather_GHSindex_data_static.R
@@ -3,7 +3,7 @@ library(readxl)
 
 static_variables_list <- c(
   "CountryCodes",
-  "Overall",
+  # "Overall",
   "Prevent",
   "Detect",
   "Respond",
@@ -24,13 +24,18 @@ static_variables_list <- c(
 
 for(i in 1:length(static_variables_list)){
   var_name <- static_variables_list[i]
+  if(var_name %in% c(  "Prevent", "Detect", "Respond", "Health", "Norms", "Risk")){
+    var_name_with_prefix <- paste0("GHS_",var_name)
+  }else{
+    var_name_with_prefix <- var_name
+  }
   assign(var_name, read_excel("GHSindex_data_static.xlsx", sheet = var_name, col_names = F))
   if(var_name == "CountryCodes"){
     assign(var_name, eval(parse(text = var_name)) %>% rename("ISO3" = "...1") )
     assign(var_name, eval(parse(text = var_name)) %>% rename("FullName" = "...2") )
   }else{
     assign(var_name, eval(parse(text = var_name)) %>% rename("FullName" = "...1") )
-    assign(var_name, eval(parse(text = var_name)) %>% rename(!!var_name := "...2") )
+    assign(var_name, eval(parse(text = var_name)) %>% rename(!!var_name_with_prefix := "...2") )
   }
   
   if(i == 1){
