df270bef203d9320aec1852b2f4eaa5170d667e9
Jay Arehart <joseph.arehart@colorado.edu>
2020-03-21 17:10:48 +0000

    Added lag factors

--- Scripts/virus_data.R
+++ Scripts/virus_data.R
@@ -11,6 +11,7 @@ devtools::install_github("RamiKrispin/coronavirus")
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(data.table)
 library(coronavirus)
 
 # Explore the data
@@ -18,7 +19,45 @@ data("coronavirus")
 head(coronavirus)
 
 # Load in csv files
-country_codes <- read.csv('InputData/CountryCodes.csv')
+country_codes <- read.csv('./InputData/CountryCodes.csv')
 
 # Add ISO country codes to project
-df = merge(coronavirus, country_codes, by.x='Country.Region', by.y='Country')
+df = tbl_df(merge(coronavirus, country_codes, by.x='Country.Region', by.y='Country'))
+
+## Looking at China's confirmed cases  -----
+CHN_ts <- df %>% filter(ISO3=='CHN') %>%
+  group_by(date, type) %>%
+  summarise(daily_cases = sum(cases)) %>%
+  pivot_wider(names_from = type,
+              values_from = daily_cases) %>%
+  ungroup() %>%
+  mutate(confirmed_cum = cumsum(confirmed)) %>%
+  mutate(death_cum = cumsum(death)) %>%
+  mutate(recovered_cum = cumsum(recovered))
+
+# Plot cummulative sum of cases
+p1 <- ggplot(CHN_ts, aes(x=date, y=confirmed_cum)) +
+  geom_line() +
+  ggtitle('Cummulative cases in China')
+
+# Plot daily number of reported cases
+p2 <- ggplot(CHN_ts, aes(x=date, y=confirmed)) +
+  geom_line() +
+  ggtitle('Reported number of cases in China')
+
+multiplot(p1, p2, cols=1)
+
+## Creating variables  -----
+
+# creating lag variables for number of reported cases, deaths, and recovered
+lags <- seq(10)   # set the number of lag factors here
+lag_names <- paste("confirmed_cum_lag", formatC(lags, width = nchar(max(lags)), flag = "0"), 
+                   sep = "_")
+lag_functions <- setNames(paste("dplyr::lag(., ", lags, ")"), lag_names)
+
+CHN <- CHN_ts %>% mutate_at(vars(confirmed, death, recovered), funs_(lag_functions)) %>%
+  select(-values1_lag1, -values1_lag2, -values2_lag1, -values2_lag2)
+
+
+
+
