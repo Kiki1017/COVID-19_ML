86584cae676d32f348557ba973973b8e6e151c5b
Chris <chris@Chris-As-MacBook-Air.local>
2020-03-21 09:27:38 -0600

    create static variable data structure

---
+++ Gather_GHSindex_data_static.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(readxl)
+
+static_variables_list <- c(
+  "CountryCodes",
+  "Overall",
+  "Prevent",
+  "Detect",
+  "Respond",
+  "Health",
+  "Norms",
+  "Risk",
+  "GDP_bill",
+  "GDP_percapita",
+  "Population_mill",
+  "HumanDevelopmentIndex_2018",
+  "EIUDemocracyIndexScore_2019",
+  "UNOnlineServicesIndexScore_2018",
+  "GlobalPeaceIndex",
+  "CorruptionsPerceptionIndex_2018",
+  "HumanCapitalIndex_2017",
+  "SDGIndexScore_2018"
+)
+
+for(i in 1:length(static_variables_list)){
+  var_name <- static_variables_list[i]
+  assign(var_name, read_excel("GHSindex_data_static.xlsx", sheet = var_name, col_names = F))
+  if(var_name == "CountryCodes"){
+    assign(var_name, eval(parse(text = var_name)) %>% rename("ISO3" = "...1") )
+    assign(var_name, eval(parse(text = var_name)) %>% rename("FullName" = "...2") )
+  }else{
+    assign(var_name, eval(parse(text = var_name)) %>% rename("FullName" = "...1") )
+    assign(var_name, eval(parse(text = var_name)) %>% rename(!!var_name := "...2") )
+  }
+  
+  if(i == 1){
+    merge_all <- eval(parse(text = static_variables_list[i]))
+  }
+  if(i > 1){
+    merge_all <- merge(merge_all, eval(parse(text = static_variables_list[i])), by = "FullName")
+  }
+}
+
+dim(merge_all)
+
+
