dfd202bd8a44156810ad8bb8e14d8efc776da14d
Jay Arehart <joseph.arehart@colorado.edu>
2020-03-27 09:44:33 +0000

    Adding script that describes workflow

---
+++ Scripts/_Master_Script.R
@@ -0,0 +1,15 @@
+# Script workflow
+
+# Collect COVID data
+source('./Scripts/virus_data.R')
+
+# Collect static variables
+source('./Scripts/Gather_GHHSindex_data_static.R')
+
+# Clean the data for implementation in the ML model
+source('./Scripts/data_cleaning.R')
+
+# Make a regression tree model
+source('real_data_tree.R')
+
+
