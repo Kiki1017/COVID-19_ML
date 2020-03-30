0774d690b446cabf83dd8f21d074eaf5203bd5d7
Jay Arehart <joseph.arehart@colorado.edu>
2020-03-30 11:41:54 +0100

    Addition of google sheets npi data.

--- Scripts/data_cleaning.R
+++ Scripts/data_cleaning.R
@@ -13,6 +13,7 @@
 # Import packages
 library(dplyr)
 library(tidyr)
+library(lubridate)
 
 # Load in raw data
 
@@ -20,10 +21,16 @@ raw_data_COVID <- read.csv('./InputData/data_COVID_2020_03_23.csv')
 
 raw_data_static <- read.csv('./InputData/data_static_vars.csv')
 
+## ---- COVID-related data cleaning and processing ----
+
 # Manipulate original datasets (e.g., normalizing)
 data_COVID <- raw_data_COVID %>%
-  select(-X)
+  select(-X) %>%
+  mutate(date = ymd(date))
+  # mutate(date = as.Date(as.character(date), format = "%m/%d/%Y"))
 
+
+## ---- Static dataset cleaning and processing ----
 data_static <- raw_data_static %>%
   # normalizing the male count by age group
   mutate(age_0_14_MalePercent = age_0_14_MaleCount / (age_0_14_MaleCount +age_15_24_MaleCount +
@@ -61,8 +68,33 @@ data_static <- raw_data_static %>%
   select(-c(age_0_14_MaleCount, age_15_24_MaleCount, age_25_54_MaleCount, age_55_64_MaleCount, age_65_plus_MaleCount,
             age_0_14_FemaleCount, age_15_24_FemaleCount, age_25_54_FemaleCount, age_55_64_FemaleCount, age_65_plus_FemaleCount))
 
-# Merge together country and 
-data_features <- left_join(data_COVID, data_static, by = 'ISO3')
+
+## ---- NPI data processing ----
+library(googlesheets4)
+# Load in data from google sheets
+ugly_url = 'https://docs.google.com/spreadsheets/d/1vrKvs52OAxuB7x2kT9r1q6IcIBxGEQsNRHsK_o7h3jo/edit#gid=284493712'
+meta_data <- sheets_get(ugly_url)
+npi_countries <- as.vector(unlist(meta_data$sheets[,1]))
+npi_countries <- npi_countries[c(-1,-2)]
+
+# Extract data from google sheets into a dataframe
+data_npi <- data.frame()
+for(i in 1:length(npi_countries)){
+  npi_df_i <- read_sheet(ugly_url, sheet = npi_countries[i]) %>%
+    mutate(date = ymd(date))
+  data_npi <- bind_rows(data_npi,npi_df_i)
+}
+glimpse(data_npi)
+summary(data_npi)
+print("These countries have data collected for non-pharmaceutical interventions: ")
+unique(data_npi$ISO3)
+
+
+## ---- Merging data sources and writing to a csv file
+# Merge together time series data (COVID and NPI)
+data_ts <- left_join(data_COVID, data_npi, by = c('date', 'ISO3'))
+
+data_features <- left_join(data_ts, data_static, by = 'ISO3')
 
 # Check merged dataframe
 sapply(data_features, function(x) sum(is.na(x)))
