70612af1d5dafaf4c261e4e140682b821d34fb7e
Madison Sankovitz <madisonsankovitz@macbook-pro-5.home>
2020-04-27 14:11:53 +0100

    Shiny

--- COVID-19_Shiny_Web_App/app.R
+++ COVID-19_Shiny_Web_App/app.R
@@ -4,25 +4,34 @@ library(shinythemes)
 library(ggplot2)
 
 # Define UI for application that displays model
-ui <- fluidPage(theme = shinytheme("simplex"),
-   
+ui <- navbarPage("The COVID Model", theme = shinytheme("simplex"),
    # Application title
-   titlePanel("The COVID Model"),
+#   titlePanel("The COVID Model"),
    
    # Sidebar with a select box input for country 
-   sidebarLayout(
-      sidebarPanel(
-         selectInput(inputId = "country",
-                     label = "Country",
-                     choices = c("United States" = "USA"))
-      ),
+#   sidebarLayout(
+#      sidebarPanel(
+#         selectInput(inputId = "country",
+#                     label = "Country",
+#                     choices = c("United States" = "USA"))
+#         radioButtons(inputId = "casesDeaths", label = "", 
+#                      choices = c("Cases" = "cases"))
+#      ),
       
-      # Show a plot of the confirmed cumulative cases per million
-      mainPanel(
-         plotOutput("plot1")
-      )
-   )
-)
+# Main panel for displaying outputs ----
+        tabPanel("Plots",
+                 fluidPage(
+         fluidRow(wellPanel(
+             selectInput(inputId = "country",
+                                 label = "Country",
+                                 choices = c("United States" = "USA"))),
+             plotOutput("plot1"),
+             plotOutput("plot2"),
+         plotOutput("plot3"),
+         plotOutput("plot4"),
+         plotOutput("plot_predict")))),
+        tabPanel("Model information", verbatimTextOutput("summary")))
+
 
 # Define server logic required to display model
 server <- function(input, output) {
@@ -30,11 +39,48 @@ server <- function(input, output) {
     # check for the input country
     if (input$country == "USA") {
       # USA
-      load(./)
+      load('./COVID-19_Shiny_Web_App/Inputs/USA_Cases_data.RData')
     }
-  })
-  }
 
+    print(plot1)
+  })
+  output$plot2 <- reactivePlot(function() {
+    # check for the input country
+    if (input$country == "USA") {
+      # USA
+      load('./COVID-19_Shiny_Web_App/Inputs/USA_Cases_data.RData')
+    }
+    
+    print(plot2)
+  })
+  output$plot3 <- reactivePlot(function() {
+    # check for the input country
+    if (input$country == "USA") {
+      # USA
+      load('./COVID-19_Shiny_Web_App/Inputs/USA_Cases_data.RData')
+    }
+    
+    print(plot3)
+  })
+  output$plot4 <- reactivePlot(function() {
+    # check for the input country
+    if (input$country == "USA") {
+      # USA
+      load('./COVID-19_Shiny_Web_App/Inputs/USA_Cases_data.RData')
+    }
+    
+    print(plot4)
+  })
+  output$plot_predict <- reactivePlot(function() {
+    # check for the input country
+    if (input$country == "USA") {
+      # USA
+      load('./COVID-19_Shiny_Web_App/Inputs/USA_Cases_data.RData')
+    }
+    
+    print(plot_predict)
+  })
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
 
